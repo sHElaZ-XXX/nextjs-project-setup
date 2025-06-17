bdbdf3069c6fb5965bc2103f5f89f725d92a2d92
sHElaZ-XXX <70573322+sHElaZ-XXX@users.noreply.github.com>
2025-06-17 18:54:10 +0200

    Add .next/static/chunks/src_app_virtual-branch_page_tsx_5e522f71._.js 1750179250123

---
+++ .next/static/chunks/src_app_virtual-branch_page_tsx_5e522f71._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ebec4c73._.js",
+  "static/chunks/node_modules_df9cc9e9._.js"
+],
+    source: "dynamic"
+});
