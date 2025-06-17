8afafd73d60440e93419fa0e4405fde1f565d7b7
sHElaZ-XXX <70573322+sHElaZ-XXX@users.noreply.github.com>
2025-06-17 18:54:24 +0200

    Add .next/static/chunks/src_app_visa-management_page_tsx_5e522f71._.js 1750179263445

---
+++ .next/static/chunks/src_app_visa-management_page_tsx_5e522f71._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_580c7f11._.js",
+  "static/chunks/node_modules_8d6971f5._.js"
+],
+    source: "dynamic"
+});
