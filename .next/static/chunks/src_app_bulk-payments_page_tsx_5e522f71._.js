3315cd58c87a94cf01629b2e70081be901571aa4
sHElaZ-XXX <70573322+sHElaZ-XXX@users.noreply.github.com>
2025-06-17 18:54:13 +0200

    Add .next/static/chunks/src_app_bulk-payments_page_tsx_5e522f71._.js 1750179253055

---
+++ .next/static/chunks/src_app_bulk-payments_page_tsx_5e522f71._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_95b71218._.js"
+],
+    source: "dynamic"
+});
